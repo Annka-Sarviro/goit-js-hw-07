63bc76b2f01d568cd6e068c4762c3892f1f34161
Ann Sarviro <Annka_nuha@i.ua>
2022-04-28 02:35:10 +0300

    test 1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryBoxEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryItems.forEach(element => {
+
+    // ПОМЕНЯТЬ href!!!!!!!!!!!!!!!
+    galleryBoxEl.insertAdjacentHTML('afterbegin', 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+        <img
+            class="gallery__image"
+            src="${element.preview}"
+            data-source="${element.original}"
+            alt="${element.description}"
+        />
+        </a>
+    </div>`)
+    
+});
+
+galleryBoxEl.addEventListener('click', onOpenModalImg);
+
+function onOpenModalImg(event) {
+    event.preventDefault();
+    const urlImgOrig = event.target.dataset.source;
+    
+    const instance = basicLightbox.create(`
+		<img width="1400" height="900" src="${urlImgOrig}">
+	`).show()
+
+
+    document.addEventListener("keydown", event => {
+        if (event.code === "Escape") {
+           document.querySelector('.basicLightbox').classList.remove('basicLightbox--visible')
+        }       
+    }) 
+    
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryBoxEl = document.querySelector('.gallery');
+
+galleryItems.forEach(element => {
+
+    // ПОМЕНЯТЬ href!!!!!!!!!!!!!!!
+    galleryBoxEl.insertAdjacentHTML('afterbegin', 
+    `<a class="gallery__item" href="${element.original}">
+    <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+  </a>`)
+    
+});
+
+galleryBoxEl.addEventListener('click', onOpenModalImg);
+
+function onOpenModalImg(event) {
+    event.preventDefault();
+    const urlImgOrig = event.target.dataset.source;
+    
+    let gallery = new SimpleLightbox('.gallery a', {
+        
+        captionDelay: 250,
+        captionSelector: 'img',
+        captionType: 'attr',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        
+    });
+}
+
